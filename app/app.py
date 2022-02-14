2aed6e2a964796e2cd2652762a23433b43b07942
Saif Rehman <saif.rehman@10pearls.com>
2022-02-14 10:35:03 +0500

    Comments Added

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,request,session
+from flask import flash
 from flask import url_for
 from flask import redirect
 from flask_session import Session
@@ -6,7 +7,7 @@ from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import random
-
+#from flask.ext.sqlalchemy import exc
 
 
 app=Flask (__name__)
@@ -20,15 +21,24 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 cursor = con.cursor()
+
+
 if __name__ == '__main__':
     session['secrrt']='sec'
 
     app.run(host="0.0.0.0", port=5000, debug=True)
 
+#Basic Function For Redirecting Login As Landing Page
+
 @app.route('/')
 def index():
     db.create_all()
     return redirect(url_for('login'))
+
+
+
+#Function For Redirecting To Employe Add Page(Get Method)
+
 @app.route('/show')
 def show():
 
@@ -36,10 +46,15 @@ def show():
     
     return redirect(url_for('eadd'))
 
+#Function For Employee Add Page
+
 @app.route('/eadd',methods=['GET','POST'])
 def eadd():
     print(session.get("name"))
     if session.get("name"):
+        try:
+            
+
 
         
     
@@ -48,37 +63,42 @@ def eadd():
 
 
     
-        if request.method=='POST':
+            if request.method=='POST':
 
 
 
 
-            print("heloo")
-            username =request.form['username']
-            password =request.form['password']
-            email=request.form['email']
-            salary=request.form['salary']
-            dept=request.form['dept']
+                print("heloo")
+                username =request.form['username']
+                password =request.form['password']
+                email=request.form['email']
+                salary=request.form['salary']
+                dept=request.form['dept']
 
                 
   
 
-            employee=Employ(username,password,email,salary,dept)
+                employee=Employ(username,password,email,salary,dept)
                
-            db.session.add(employee)
-            db.session.commit()
+                db.session.add(employee)
+                db.session.commit()
                 
                
 
-            return redirect(url_for('show'))
-        else:
+                return redirect(url_for('show'))
+            else:
+                edata=Employ.query.filter()
+                return render_template('employee.html',Result=edata)
+        except Exception as e:
             edata=Employ.query.filter()
+            print('yo',str(e))
             return render_template('employee.html',Result=edata)
+
     else:
         
         return redirect(url_for('login'))
 
-                         
+#Function For Employee Delete                   
 
 @app.route('/edel/<username>',methods=['GET','POST'])
 def edel(username):
@@ -102,6 +122,9 @@ def edel(username):
             return redirect(url_for('eadd'))
     else:
         return redirect(url_for('login'))
+
+
+#Function For Employee Update 
 
 @app.route('/eupdate',methods=['GET','POST'])
 def eupdate():
@@ -127,10 +150,11 @@ def eupdate():
 
         
 
-    
+#Function For Employee Login  
 
 @app.route('/login',methods=['POST','GET'])
 def login():
+    error=None
 
 
 
@@ -161,23 +185,31 @@ def login():
 
 
                 print(i.username)
+                
+
                 if request.form['username'] == i.username and request.form['password'] == i.password:
                     print('saifyyy')
+                    
                     session["name"] = request.form.get("username")
+
 
                     
 
                     return redirect(url_for('eadd'))
                     
                 else:
+                   
                     return render_template('login.html')
                     
         else:
 
 
             return render_template('login.html')
-    except:
-        return render_template('login.html')
+    except Exception as e:
+
+        #print(e,'EKKK')
+        # return "error hello", 400
+        return render_template("500.html", error = str(e))
         
 
 
